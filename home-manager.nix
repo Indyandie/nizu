b1d34629b87151345be7bc4b3bf9add501688981
indyandie <1058837+Indyandie@users.noreply.github.com>
2023-09-08 22:40:00 -0500

    home-manager & hyprland

---
+++ home-manager.nix
@@ -0,0 +1,47 @@
+# home-manager
+
+{ config, pkgs, username, ... }:
+
+{
+  home-manager.useGlobalPkgs = true;
+
+  home-manager.users.${username} = { pkgs, ...}: {
+		 home = {
+      stateVersion = "23.05";
+      packages = with pkgs; [
+        dconf
+        # gnome.dconf-editor
+      ];
+
+      pointerCursor = {
+        name = "Simp1e-Gruvbox-Light";
+        package = pkgs.simp1e-cursors;
+        size = 24;
+        gtk.enable = true;
+        x11.enable = true;
+      };
+    };
+
+
+    gtk = {
+      enable = true;
+
+      font = {
+        size = 16;
+        name = "JetBrainsMono Nerd Font";
+      };
+
+      gtk3 = {
+        extraConfig = {
+          gtk-application-prefer-dark-theme = 1;
+        };
+      };
+
+      gtk4 = {
+        extraConfig = {
+          gtk-application-prefer-dark-theme = 1;
+        };
+      };
+    };
+  };
+}
---
+++ hyprland.nix
@@ -0,0 +1,39 @@
+# hyprland
+
+{ config, pkgs, ... }:
+
+{
+  programs.hyprland = {
+    enable = true;
+    xwayland = {
+      enable = true;
+      hidpi = true;
+    };
+  };
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+
+    # make gtk apps happy
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
+  environment.systemPackages = with pkgs; [
+		
+		hyprland
+		wayland
+		xwayland
+		xdg-utils # opening default programs from links
+		xdg-desktop-portal  
+		xdg-desktop-portal-hyprland
+		waybar
+		wofi
+		eww
+		eww-wayland
+		alacritty
+		kitty
+		seatd
+		cliphist
+	]
+}

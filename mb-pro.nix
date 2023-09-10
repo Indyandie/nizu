7523c598ae075bafa487220d903705783e4c3962
indyandie <1058837+Indyandie@users.noreply.github.com>
2023-09-09 22:00:37 -0500

    more mbp configs

--- mb-pro.nix
+++ mb-pro.nix
@@ -8,7 +8,7 @@
     [ # Include the results of the hardware scan.
       # <nixos-hardware/apple/macbook-pro/11-5>
     ];
-
+  
   # wifi mac
   # boot.kernelModules = [ "wl" ];
   networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -27,9 +27,25 @@
   # networking.enableB43Firmware = true;
 
   powerManagement.enable = true;
+  services.acpid.enable = true;
+  powerManagement.cpuFreqGovernor = "schedutil";
+  services.mbpfan.enable = true;
+  hardware.cpu.intel.updateMicrocode = true;
+  hardware.opengl.extraPackages = with pkgs; [
+    vaapiIntel
+    vaapiVdpau
+    libvdpau-va-gl
+    intel-media-driver
+  ];
+
+  boot = {
+  kernelModules = [ "applesmc" "i915" ];
+    # https://forum.manjaro.org/t/kworker-kacpid-over-70-of-cpu-dual-boot-mac-manjaro/61981
+    kernelParams = [ "hid_apple.iso_layout=0" "acpi_backlight=vendor" "acpi_mask_gpe=0x15" ];
+  };
 
   # for mouse driver
-  services.xserver.synaptics.enable = true;
+  # services.xserver.synaptics.enable = true;
 
   # Enable CUPS to print documents.
   services.printing.enable = true;

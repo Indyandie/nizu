b87d6ce638c6f52bfc28918294732edab8856621
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-05-20 20:23:01 -0500

    style: format

--- configuration.nix
+++ configuration.nix
@@ -81,210 +81,210 @@
   home-manager.useGlobalPkgs = true;
 
   home-manager.users.nizusan = { pkgs, ... }: {
-      home = {
-        packages = [ ];
-        stateVersion = "23.11";
+    home = {
+      packages = [ ];
+      stateVersion = "23.11";
+    };
+
+    gtk = {
+      enable = true;
+
+      font = {
+        size = 16;
+        name = "JetBrainsMono Nerd Font";
       };
 
-      gtk = {
-        enable = true;
-
-        font = {
-          size = 16;
-          name = "JetBrainsMono Nerd Font";
+      gtk3 = {
+        extraConfig = {
+          gtk-application-prefer-dark-theme = 1;
         };
+      };
 
-        gtk3 = {
-          extraConfig = {
-            gtk-application-prefer-dark-theme = 1;
-          };
-        };
-
-        gtk4 = {
-          extraConfig = {
-            gtk-application-prefer-dark-theme = 1;
-          };
+      gtk4 = {
+        extraConfig = {
+          gtk-application-prefer-dark-theme = 1;
         };
       };
     };
+  };
 
-    # Allow unfree packages
-    nixpkgs.config.allowUnfree = true;
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
-    # List packages installed in system profile. To search, run:
-    # $ nix search wget
-    environment.systemPackages = with pkgs; [
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
 
-      # base
-      vim
-      helix
-      curl
-      wget
-      git
-      starship
-      neofetch
-      zsh
+    # base
+    vim
+    helix
+    curl
+    wget
+    git
+    starship
+    neofetch
+    zsh
 
-      # files and drives
-      udisks
-      udisks2
-      ntfs3g
-      exfat
-      cargo
-      rustc
-      rust-analyzer
+    # files and drives
+    udisks
+    udisks2
+    ntfs3g
+    exfat
+    cargo
+    rustc
+    rust-analyzer
 
-      dunst
-      zip
-      unzip
-      killall
-      tldr
-      flatpak
-      bat
-      bat-extras.prettybat
-      bat-extras.batwatch
-      bat-extras.batman
-      bat-extras.batpipe
-      seatd
-      networkmanager
-      networkmanagerapplet
+    dunst
+    zip
+    unzip
+    killall
+    tldr
+    flatpak
+    bat
+    bat-extras.prettybat
+    bat-extras.batwatch
+    bat-extras.batman
+    bat-extras.batpipe
+    seatd
+    networkmanager
+    networkmanagerapplet
+  ];
+
+  # run a non-nixos executable on NixOs
+  # https://unix.stackexchange.com/questions/522822/different-methods-to-run-a-non-nixos-executable-on-nixos
+  programs.nix-ld.enable = true;
+
+  environment.variables = {
+    NIX_LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
+      stdenv.cc.cc
+      openssl
+      xorg.libXcomposite
+      xorg.libXtst
+      xorg.libXrandr
+      xorg.libXext
+      xorg.libX11
+      xorg.libXfixes
+      libGL
+      libva
+      pipewire.lib
+      xorg.libxcb
+      xorg.libXdamage
+      xorg.libxshmfence
+      xorg.libXxf86vm
+      libelf
+
+      # Required
+      glib
+      gtk2
+      bzip2
+
+      # Without these it silently fails
+      xorg.libXinerama
+      xorg.libXcursor
+      xorg.libXrender
+      xorg.libXScrnSaver
+      xorg.libXi
+      xorg.libSM
+      xorg.libICE
+      gnome2.GConf
+      nspr
+      nss
+      cups
+      libcap
+      SDL2
+      libusb1
+      dbus-glib
+      ffmpeg
+      # Only libraries are needed from those two
+      libudev0-shim
+
+      # Verified games requirements
+      xorg.libXt
+      xorg.libXmu
+      libogg
+      libvorbis
+      SDL
+      SDL2_image
+      glew110
+      libidn
+      tbb
+
+      # Other things from runtime
+      flac
+      freeglut
+      libjpeg
+      libpng
+      libpng12
+      libsamplerate
+      libmikmod
+      libtheora
+      libtiff
+      pixman
+      speex
+      SDL_image
+      SDL_ttf
+      SDL_mixer
+      SDL2_ttf
+      SDL2_mixer
+      libappindicator-gtk2
+      libdbusmenu-gtk2
+      libindicator-gtk2
+      libcaca
+      libcanberra
+      libgcrypt
+      libvpx
+      librsvg
+      xorg.libXft
+      libvdpau
+      gnome2.pango
+      cairo
+      atk
+      gdk-pixbuf
+      fontconfig
+      freetype
+      dbus
+      alsaLib
+      expat
+      # Needed for electron
+      libdrm
+      mesa
+      libxkbcommon
     ];
+    NIX_LD = lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
+  };
 
-    # run a non-nixos executable on NixOs
-    # https://unix.stackexchange.com/questions/522822/different-methods-to-run-a-non-nixos-executable-on-nixos
-    programs.nix-ld.enable = true;
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-    environment.variables = {
-      NIX_LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
-        stdenv.cc.cc
-        openssl
-        xorg.libXcomposite
-        xorg.libXtst
-        xorg.libXrandr
-        xorg.libXext
-        xorg.libX11
-        xorg.libXfixes
-        libGL
-        libva
-        pipewire.lib
-        xorg.libxcb
-        xorg.libXdamage
-        xorg.libxshmfence
-        xorg.libXxf86vm
-        libelf
+  # List services that you want to enable:
 
-        # Required
-        glib
-        gtk2
-        bzip2
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
 
-        # Without these it silently fails
-        xorg.libXinerama
-        xorg.libXcursor
-        xorg.libXrender
-        xorg.libXScrnSaver
-        xorg.libXi
-        xorg.libSM
-        xorg.libICE
-        gnome2.GConf
-        nspr
-        nss
-        cups
-        libcap
-        SDL2
-        libusb1
-        dbus-glib
-        ffmpeg
-        # Only libraries are needed from those two
-        libudev0-shim
-
-        # Verified games requirements
-        xorg.libXt
-        xorg.libXmu
-        libogg
-        libvorbis
-        SDL
-        SDL2_image
-        glew110
-        libidn
-        tbb
-
-        # Other things from runtime
-        flac
-        freeglut
-        libjpeg
-        libpng
-        libpng12
-        libsamplerate
-        libmikmod
-        libtheora
-        libtiff
-        pixman
-        speex
-        SDL_image
-        SDL_ttf
-        SDL_mixer
-        SDL2_ttf
-        SDL2_mixer
-        libappindicator-gtk2
-        libdbusmenu-gtk2
-        libindicator-gtk2
-        libcaca
-        libcanberra
-        libgcrypt
-        libvpx
-        librsvg
-        xorg.libXft
-        libvdpau
-        gnome2.pango
-        cairo
-        atk
-        gdk-pixbuf
-        fontconfig
-        freetype
-        dbus
-        alsaLib
-        expat
-        # Needed for electron
-        libdrm
-        mesa
-        libxkbcommon
-      ];
-      NIX_LD = lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
-    };
-
-    # Some programs need SUID wrappers, can be configured further or are
-    # started in user sessions.
-    # programs.mtr.enable = true;
-    # programs.gnupg.agent = {
-    #   enable = true;
-    #   enableSSHSupport = true;
-    # };
-
-    # List services that you want to enable:
-
-    # Enable the OpenSSH daemon.
-    services.openssh.enable = true;
-
-    # Open ports in the firewall.
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # networking.firewall.enable = false;
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
 
-    #  ___ __  __ ____   ___  ____ _____  _    _   _ _____ _ 
-    # |_ _|  \/  |  _ \ / _ \|  _ \_   _|/ \  | \ | |_   _| |
-    #  | || |\/| | |_) | | | | |_) || | / _ \ |  \| | | | | |
-    #  | || |  | |  __/| |_| |  _ < | |/ ___ \| |\  | | | |_|
-    # |___|_|  |_|_|    \___/|_| \_\|_/_/   \_\_| \_| |_| (_)
+  #  ___ __  __ ____   ___  ____ _____  _    _   _ _____ _ 
+  # |_ _|  \/  |  _ \ / _ \|  _ \_   _|/ \  | \ | |_   _| |
+  #  | || |\/| | |_) | | | | |_) || | / _ \ |  \| | | | | |
+  #  | || |  | |  __/| |_| |  _ < | |/ ___ \| |\  | | | |_|
+  # |___|_|  |_|_|    \___/|_| \_\|_/_/   \_\_| \_| |_| (_)
 
-    # USE THE VALUE IN THE INIT configuration.nix created on install
-    # This value determines the NixOS release from which the default
-    # settings for stateful data, like file locations and database versions
-    # on your system were taken. It‘s perfectly fine and recommended to leave
-    # this value at the release version of the first install of this system.
-    # Before changing this value read the documentation for this option
-    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    # system.stateVersion = "23.05"; # Did you read the comment?
-  }
+  # USE THE VALUE IN THE INIT configuration.nix created on install
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  # system.stateVersion = "23.05"; # Did you read the comment?
+}

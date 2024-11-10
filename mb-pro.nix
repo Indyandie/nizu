91378317c3c0b9f341b1a96e58adc9c2ffd0085a
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-11-09 20:32:58 -0600

    refactor(macbook-pro): tidy up and tinker the macbook-pro config

--- mb-pro.nix
+++ mb-pro.nix
@@ -1,116 +1,91 @@
-# Let's see how this goes :O
-
 { config, pkgs, lib, ... }:
-
 {
 
-  imports =
-    [
-      # Include the results of the hardware scan.
-      # <nixos-hardware/apple/macbook-pro/11-5>
-      <nixos-hardware/apple>
-    ];
-
-  systemd.packages = with pkgs; [
-    auto-cpufreq
+  imports = [
+    # Include the results of the hardware scan.
+    <nixos-hardware/apple/macbook-pro/11-5>
+    <nixos-hardware/apple>
   ];
+
+  # systemd.packages = with pkgs; [
+  #   auto-cpufreq
+  # ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
-  # wifi mac
-  # boot.kernelModules = [ "wl" ];
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.firewall.enable = true;
-  # boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+  # https://forum.manjaro.org/t/kworker-kacpid-over-70-of-cpu-dual-boot-mac-manjaro/61981
+  boot = {
+    kernelModules = [ "applesmc" "i915" "wl" ];
+    kernelParams = [ "hid_apple.iso_layout=0" "acpi_backlight=vendor" "acpi_mask_gpe=0x15" ];
+    extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+    blacklistedKernelModules = [ "nouveau" "nvidia" ]; # Disable NVIDIA video cards
+    initrd.kernelModules = [ "amdgpu" ];
+  };
 
-  # The wifi broadcom driver 
-  networking.enableB43Firmware = true;
+  # wifi
+  networking = {
+    firewall.enable = true;
+    enableB43Firmware = true; # The wifi broadcom driver     
+  };
 
   hardware.bluetooth.enable = true;
   hardware.facetimehd.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # The wifi broadcom driver 
-  # networking.enableB43Firmware = true;
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  # hardware.cpu.intel.updateMicrocode = true;
 
   powerManagement.enable = true;
-  services.acpid.enable = true;
-  # powerManagement.cpuFreqGovernor = "schedutil";
   powerManagement.cpuFreqGovernor = "ondemand";
+  # powerManagement.cpuFreqGovernor = "schedutil";
+
+  services.acpid.enable = true;
   services.mbpfan.enable = true;
   services.auto-cpufreq.enable = true;
-  # hardware.cpu.intel.updateMicrocode = true;
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-  boot.blacklistedKernelModules = [ "nouveau" "nvidia" ]; # Disable NVIDIA video cards
 
   # Accelerated Video Playback
 
   nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
     vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
   };
 
   hardware.opengl = {
     enable = true;
+    #   #   ## radv: an open-source Vulkan driver from freedesktop
     driSupport = true;
     driSupport32Bit = true;
     extraPackages = with pkgs; [
-      intel-compute-runtime
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      vaapiVdpau
-      libvdpau-va-gl
+      ## amdvlk: an open-source Vulkan driver from AMD
       amdvlk
-      intel-ocl
+
+      ## vaapi
       # intel-vaapi-driver
-      rocmPackages.clr.icd
+      # vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      #   intel-compute-runtime
+
+      #   intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      #   intel-ocl
+      #   vaapiVdpau
+      #   libvdpau-va-gl
+      #   rocmPackages.clr.icd
     ];
+
+    # amd drivers
+    extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
   };
 
-  # hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ 
-  #   vaapiIntel 
-  #   intel-media-driver
-  #   intel-vaapi-driver
-  #   amdvlk
-  # ];
-
-  # opencl
   environment.variables = {
     ROC_ENABLE_PRE_VEGA = "1";
   };
 
-  boot.initrd.kernelModules = [ "amdgpu" ];
   services.xserver.videoDrivers = [ "amdgpu" ];
-
-  # # amd drivers
-
-  # hardware.opengl = {
-  #   ## radv: an open-source Vulkan driver from freedesktop
-  #   driSupport = true;
-  #   driSupport32Bit = true;
-
-  #   ## amdvlk: an open-source Vulkan driver from AMD
-  #   extraPackages = [ pkgs.amdvlk ];
-  #   extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
-  # };
 
   # pkgs
   environment.systemPackages = with pkgs; [
-    # vaapi
     libva
     libva-utils
     libaom
     mesa
   ];
-
-  # https://forum.manjaro.org/t/kworker-kacpid-over-70-of-cpu-dual-boot-mac-manjaro/61981
-  boot = {
-    kernelModules = [ "applesmc" "i915" ];
-    kernelParams = [ "hid_apple.iso_layout=0" "acpi_backlight=vendor" "acpi_mask_gpe=0x15" ];
-  };
-
-  # for mouse driver
-  # services.xserver.synaptics.enable = true;
 
   # Enable CUPS to print documents.
   services.printing.enable = true;

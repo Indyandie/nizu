e0eef65c0f402967fc95d4fbe5a5fd17fae3f359
indyandie <1058837+Indyandie@users.noreply.github.com>
2025-01-06 22:49:54 -0600

    chore(flake): remove unused comments, add comments for future updates

--- flake.nix
+++ flake.nix
@@ -6,11 +6,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # zen-browser.url = "github:MarceColl/zen-browser-flake";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, nixpkgs-unstable, ... }@attrs:
+  outputs =
+    { self
+    , nixpkgs
+    , nixos-hardware
+    , nixpkgs-unstable
+      # inputs
+    , ...
+    }@attrs:
     {
       nixosConfigurations.nizusama = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -20,6 +26,7 @@
             nixpkgs.overlays = [
               (final: prev: {
                 # unstable = nixpkgs-unstable.legacyPackages.${prev.system};
+                # OR
                 # use this variant if unfree packages are needed:
                 unstable = import nixpkgs-unstable {
                   # inherit prev.system;
@@ -31,6 +38,12 @@
           }
           nixos-hardware.nixosModules.apple-macbook-pro-11-5
           ./configuration.nix
+          # {
+          #   environment.systemPackages = [
+          #     # output packages
+          #     ghostty.packages.x86_64-linux.default
+          #   ];
+          # }
         ];
       };
     };

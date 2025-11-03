bc93d8590749d90ca7169086ad8f3ff8b233154f
indyandie <1058837+Indyandie@users.noreply.github.com>
2025-11-02 21:09:04 -0600

    modules(steam): enable `hardware.steam-hardware`

--- steam.nix
+++ steam.nix
@@ -3,8 +3,12 @@
 {
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    protontricks.enable = true;
+
+    localNetworkGameTransfers.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    remotePlay.openFirewall = true;
   };
+
+  hardware.steam-hardware.enable = true;
 }

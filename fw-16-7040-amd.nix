cc9706460804ffc8f68ec2c855a045cfaa4dec93
indyandie <1058837+Indyandie@users.noreply.github.com>
2025-11-11 23:19:20 -0600

    services(framework): add `udev-extraRules` to prevent wakeup
    https://wiki.nixos.org/wiki/Hardware/Framework/Laptop_16

--- fw-16-7040-amd.nix
+++ fw-16-7040-amd.nix
@@ -20,4 +20,19 @@
     serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
   };
 
+
+  ## prevent wakeup
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="32ac", ATTRS{idProduct}=="0012", ATTR{power/wakeup}="disabled", ATTR{driver/1-1.1.1.4/power/wakeup}="disabled"
+    SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="32ac", ATTRS{idProduct}=="0014", ATTR{power/wakeup}="disabled", ATTR{driver/1-1.1.1.4/power/wakeup}="disabled"
+  '';
+
+  ## Useful Utilities
+  # inputmodule.enable = true;
+
+  ## display the time on your inputmodule
+
+  ###```shell
+  #inputmodule-control   --serial-dev /dev/ttyACM1 led-matrix --clock
+  ###```
 }
